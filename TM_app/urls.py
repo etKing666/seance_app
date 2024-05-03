97c56edf5c6e2b63f0847b42399cd5639324afa4
etking <etkingetir@gmail.com>
2024-05-03 09:57:35 +0300

    Final version

--- TM_app/urls.py
+++ TM_app/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('seance/', include('seance.urls'))
+    path('', include('seance.urls'))
 ]
 
 if settings.DEBUG:
