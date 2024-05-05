2742a48b458f6175d27e42df4737eee46b04901f
etking <etkingetir@gmail.com>
2024-05-05 22:39:16 +0300

    Tests added.

--- seance/tests/test_models.py
+++ seance/tests/test_models.py
@@ -5,11 +5,13 @@ from seance.models import Questions, Suggestions
 class QuestionsTestCase(TestCase):
     def create_question(self, qid=999, step="9.9", question="Test question", qtype=5, parent=False, children="", pid=0,
                         section=1, value=1, factor=1, dfd=False):
+        """ Creates a Questions object"""
         return Questions.objects.create(qid=qid, step=step, question=question, qtype=qtype, parent=parent,
                                         children=children, pid=pid, section=section, value=value, factor=factor,
                                         dfd=dfd)
 
     def test_questions(self):
+        """ Tests if created Questions object is an instance of Questions class"""
         q = self.create_question()
         self.assertTrue(isinstance(q, Questions))
         self.assertEqual(q.qid, 999)
@@ -18,10 +20,12 @@ class QuestionsTestCase(TestCase):
 class SuggestionsTestCase(TestCase):
     def create_suggestion(self, sid=9999, rquid=999, risk="Test risk", action="Test action", sources="Test source",
                           section=1):
+        """ Creates a Suggestions object"""
         return Suggestions.objects.create(sid=sid, rquid=rquid, risk=risk, action=action, sources=sources,
                                           section=section)
 
     def test_suggestions(self):
+        """ Tests if created Suggestions object is an instance of Suggestions class """
         s = self.create_suggestion()
         self.assertTrue(isinstance(s, Suggestions))
         self.assertEqual(s.sid, 9999)
